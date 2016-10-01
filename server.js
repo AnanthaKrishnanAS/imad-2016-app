f89fbfc0edc831458bc06567a7a4ced124becee8
AnanthaKrishnanAS <ananthakrishnan25252015@gmail.com>
2016-10-01 20:18:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article'));
 });
-app.get('/',function(req,res){
+app.get('/article1',function(req,res){
     res.sendfile(__dirname,'ui','article1.html');
 })
 
